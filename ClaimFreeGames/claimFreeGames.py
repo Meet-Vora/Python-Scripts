3325815eecafdfd73048edd7e840d79f8ef071ff
Meet Vora <meetrajivvora@gmail.com>
2020-08-29 16:56:21 -0700

    Little slow, but claimed all games on epic store!

--- ClaimFreeGames/claimFreeGames.py
+++ ClaimFreeGames/claimFreeGames.py
@@ -11,86 +11,136 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
 
 
-
 # go to epic games store website where they list all the free games
 # Get a list of all the free games available
-# Click on each one, and then grab them for free 
+# Click on each one, and then grab them for free
 
 BASE_URL = "https://www.epicgames.com"
 FREEGAME_URL_EXTENSION = "/store/en-US/free-games"
 CHROME_DRIVER_PATH = "./chromedriver_linux"
-CLASSNAMES = ["css-1oymjns-CardGridDesktopPortrait__cardWrapperDesktop", 
-              "css-1qv6lea-CardGridDesktopLandscape__cardWrapperDesktop", 
+CLASSNAMES = ["css-1oymjns-CardGridDesktopPortrait__cardWrapperDesktop",
+              "css-1qv6lea-CardGridDesktopLandscape__cardWrapperDesktop",
               "css-1adx3p4-BrowseGrid-styles__card"]
+CONSENT_TEXT = "This game contains mature content recommended only for ages 18+"
+
 
 def claim_games(game_link):
-    # for url in get_game_links():
     driver = setup_driver(game_link)
-    time.sleep(10)
-    wait = WebDriverWait(driver, 30 , poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, 
-        ElementNotSelectableException, NoSuchElementException])
 
-    button_xpath = "/html/body/div/div/div[4]/main/div/div/div[2]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[3]/div"
-    pprint(driver.find_element_by_xpath("/html/body/div/div/div[4]/main/div[2]/div/div[1]/p").text)
+    # Wait for elements in game link webpage to load
+    time.sleep(5)
+
+    # consent_button_xpath = "/html/body/div/div/div[4]/main/div[2]/div/div[1]/p"
+    consent_paragraph_xpath = "//*[contains(text(), '{0}')]".format(
+        CONSENT_TEXT)
+    consent_button_xpath = "/html/body/div/div/div[4]/main/div[2]/div/div[2]/div/button"
+    consent_paragraph = None
+
+    # claim_button_xpath = "/html/body/div/div/div[4]/main/div/div/div[2]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[3]/div"
+    # claim_button_xpath = "/html/body/div[1]/div/div[4]/main/div/div/div[2]/div/div[2]/div[2]/div/div/div[3]" \
+    #     "/div/div/div/div[3]/div/button"
+    claim_button_xpath = "//button[contains(., 'Get')]"
+    claim_button_text_xpath = claim_button_xpath + "/span/span"
+    claim_button_text = None
+
+    placeorder_button_expath = "/html/body/div[3]/div/div/div[4]/div/div[4]/div[1]/div[2]/div[5]/div/div/button"
+
+    try:
+        consent_paragraph = driver.find_element_by_xpath(
+            consent_paragraph_xpath)
+        if consent_paragraph is not None:
+            click_button(driver, consent_button_xpath)
+
+    except NoSuchElementException:
+        print("No consent button")
+
+    click_button(driver, claim_button_xpath)
+    click_button(driver, placeorder_button_expath)
+
     # if driver.find_element_by_xpath("/html/body/div/div/div[4]/main/div[2]/div/div[1]/p/text()") != "":
     #     button_xpath = "/html/body/div/div/div[4]/main/div[2]/div/div[2]/div/button"
-    
-    # button = wait.until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
-    # button.click()
-    # print("clicked")
+
+    # try:
+    #     claim_button_text = driver.find_element_by_xpath(
+    #         claim_button_text_xpath)
+    #     # print("BUTTON TEXT:", claim_button_text.text)
+    #     if claim_button_text.text.lower() == "get":
+    #         click_button(driver, claim_button_xpath)
+    #         click_button(driver, placeorder_button_expath)
+
+    # except NoSuchElementException:
+    #     print("Button is not get button, or place order button doesn't work")
+    # claim_button = wait.until(
+    #     EC.element_to_be_clickable((By.XPATH, claim_button_xpath)))
+    # claim_button.click()
+    # # print("clicked")
     # time.sleep(15)
     driver.quit()
-    print("done")
 
-
+    print("Got game")
+    print("=====================")
 
     # game_div.click()
     # burner = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, CLASSNAMES[0])))
 
 
+def click_button(driver, xpath):
+    # wait = WebDriverWait(driver, 30, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException,
+    #    ElementNotSelectableException])
+    print("XPATH:", xpath)
+    button = None
+    try:
+        # button = wait.until(EC.element_to_be_clickable(
+        #     (By.XPATH, xpath)))
+        button = driver.find_element_by_xpath(xpath)
+        if button is not None:
+            button.click()
+            time.sleep(3)
+    except NoSuchElementException:
+        print("DIDN'T WORK")
+
 
 def get_game_links():
     driver = setup_driver()
-        # driver = setup_driver()
-    # temp_classes = ["css-1oymjns-CardGridDesktopPortrait__cardWrapperDesktop"]
+    # time.sleep(4)
 
     game_divs, game_links = [], []
     for className in CLASSNAMES:
         game_divs += driver.find_elements_by_class_name(className)
 
     for game_div in game_divs:
-        soup = BeautifulSoup(game_div.get_attribute('innerHTML'), "html.parser")
+        soup = BeautifulSoup(game_div.get_attribute(
+            'innerHTML'), "html.parser")
         game_links += [BASE_URL + soup.find('a')['href']]
 
     driver.quit()
-    print(game_links)
-    # for game_link in game_links[0]:
-    claim_games(game_links[0])
 
+    for game_link in game_links:
+        claim_games(game_link)
     print("Claimed all games!")
 
-        # for elem in games:
-        #     # elem = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, className)))
-        #     elem.click()
-        #     # button_class = "css-zc5dwj-Button-styles__main"
-        #     button_class = "css-zc5dwj-Button-styles__main"
-        #     button_xpath = "/html/body/div/div/div[4]/main/div/div/div[2]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[3]/div"
+    # for elem in games:
+    #     # elem = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, className)))
+    #     elem.click()
+    #     # button_class = "css-zc5dwj-Button-styles__main"
+    #     button_class = "css-zc5dwj-Button-styles__main"
+    #     button_xpath = "/html/body/div/div/div[4]/main/div/div/div[2]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[3]/div"
 
-        #     button = wait.until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
-        #     button.click()
-        #     print("done")
-        #     driver.quit()
+    #     button = wait.until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
+    #     button.click()
+    #     print("done")
+    #     driver.quit()
 
-            # button_class = "css-14i612r-PurchaseButton-styles__ctaButtons"
-            # button_xpath = "/html/body/div/div/div[4]/main/div/div/div[2]/div/div[2]/div[4]/div/div/div/div[3]/div/button"
-            # driver.implicitly_wait(45)
-            # button = driver.find_elements_by_xpath(button_xpath)[0]
-            # button = WebDriverWait(driver, 45).until(find(driver, button_xpath))
-            # print("done waiting")
-            # print(button_xpath)
-            # # driver.implicitly_wait(15)
-            # print("waiting...")
-            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
+    # button_class = "css-14i612r-PurchaseButton-styles__ctaButtons"
+    # button_xpath = "/html/body/div/div/div[4]/main/div/div/div[2]/div/div[2]/div[4]/div/div/div/div[3]/div/button"
+    # driver.implicitly_wait(45)
+    # button = driver.find_elements_by_xpath(button_xpath)[0]
+    # button = WebDriverWait(driver, 45).until(find(driver, button_xpath))
+    # print("done waiting")
+    # print(button_xpath)
+    # # driver.implicitly_wait(15)
+    # print("waiting...")
+    # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
 
 
 # def find(driver, className):
@@ -101,7 +151,6 @@ def get_game_links():
 #         return False
 # element = WebDriverWait(driver, secs).until(find)
 
-
     # test_class = "css-1oymjns-CardGridDesktopPortrait__cardWrapperDesktop"
     # elem = driver.find_elements_by_class_name(test_class)
     # print(elem)
@@ -111,8 +160,6 @@ def get_game_links():
     # # driver.find_element_by_class_name(test_class)
     # # driver.implicitly_wait(15)
 
-    
-    
     # response = requests.get(url)
     # # print(url, response.status_code)
     # # tree = html.fromstring(response.content)
@@ -153,6 +200,15 @@ def setup_driver(url=BASE_URL + FREEGAME_URL_EXTENSION):
     print(url)
     return driver
 
+
+def test():
+    xpath = "//button[contains(., 'Get')]"
+    driver = setup_driver(
+        "https://www.epicgames.com/store/en-US/product/paladins/home")
+    click_button(driver, xpath)
+
+
 # driver = setup_driver()
 # driver.get(BASE_URL)
 get_game_links()
+# test()
